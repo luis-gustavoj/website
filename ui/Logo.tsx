38d8a80adb88c461294b970a72a900d1a9c285d9
Luis <luisg.juliao@gmail.com>
2023-11-30 10:02:45 -0300

    fea: Add Logo component

---
+++ ui/Logo.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+
+type LogoProps = {
+  className?: string;
+};
+
+export const Logo = ({ className }: LogoProps) => (
+  <p
+    className={clsx(
+      "tracking-tighter font-bold text-lg leading-none font-mono",
+      className
+    )}
+  >
+    luisgustavosilva.
+  </p>
+);
