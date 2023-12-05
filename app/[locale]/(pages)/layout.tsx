eb60bcc513e7fa2e812bbac20fa50badfaf5ca8f
Luis <luisg.juliao@gmail.com>
2023-12-05 16:56:23 -0300

    fix: pointer events none on wave svg

--- app/[locale]/(pages)/layout.tsx
+++ app/[locale]/(pages)/layout.tsx
@@ -14,7 +14,7 @@ export default function PagesLayout({
         {children}
       </main>
       <Footer />
-      <div className="flex absolute bottom-0 left-0 w-full overflow-x-hidden">
+      <div className="flex absolute bottom-0 left-0 w-full overflow-x-hidden pointer-events-none">
         <WaveSVG className="relative fill-zinc-200 dark:fill-zinc-900 -z-10 min-w-[1440px] max-md:-left-1/4" />
       </div>
     </>
